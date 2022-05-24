e46f8210f48224fba45b08b715d3f33cd04ddbd8
thomaswening <thomaswening94@gmail.com>
2022-05-24 19:59:14 +0200

    rename class Address to Contact

---
+++ PhoneBook/PhoneBook/Contact.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneBook
+{
+    internal class Contact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleNames { get; set; }
+        public ulong? MobileNumber { get; set; }
+        public ulong? LandlineNumber { get; set; }
+        public int? Prefix { get; set; }
+        public string Notes { get; set; }
+
+        public Contact()
+        {
+            FirstName = string.Empty;
+            LastName = string.Empty;
+            MiddleNames = string.Empty;
+            Notes = string.Empty;
+        }
+    }
+}
