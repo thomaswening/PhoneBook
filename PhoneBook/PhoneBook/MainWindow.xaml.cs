256dd53833709b156271dcddd6fe7641b0332456
thomaswening <thomaswening94@gmail.com>
2022-05-24 20:00:26 +0200

    add datagrid to view, add SqlConnection to database and bind data to datagrid

--- PhoneBook/PhoneBook/MainWindow.xaml.cs
+++ PhoneBook/PhoneBook/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,9 +25,45 @@ namespace PhoneBook
         public MainWindow()
         {
             InitializeComponent();
+            Bind();
         }
 
-        public void Bind() { }
+        public void Bind() 
+        {
+            string CONNECTION_STRING = @"Server=THOMAS-HOME;Database=phone_book;Trusted_Connection=True;";
+            string QUERY = @"SELECT * FROM contacts";
+            DataSet dataSet = new();
+
+            try
+            {
+                using (SqlConnection sqlConnection = new(CONNECTION_STRING))
+                {
+                    SqlCommand sqlCommand = new(QUERY, sqlConnection);
+                    SqlDataAdapter sqlDataAdapter = new(sqlCommand);
+                    sqlDataAdapter.Fill(dataSet, "contacts");
+                }
+
+                DataTable contactsTable = dataSet.Tables["contacts"];
+                contactsTable.Columns.Remove("id");
+                contactsTable.Columns.Remove("middle_names");
+                contactsTable.Columns.Remove("prefix");
+                contactsTable.Columns.Remove("notes");
+
+                contactsTable.Columns["last_name"].ColumnName = "Last name";
+                contactsTable.Columns["first_name"].ColumnName = "First name";
+                contactsTable.Columns["mobile_number"].ColumnName = "Mobile number";
+                contactsTable.Columns["landline_number"].ColumnName = "Landline number";
+
+                DataView dataView = new(contactsTable, string.Empty, "Last name", DataViewRowState.CurrentRows);
+
+                grdContacts.ItemsSource = dataView;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Exception: {ex.Message}");
+            }
+        }
 
         public void Unbind() { }
 
